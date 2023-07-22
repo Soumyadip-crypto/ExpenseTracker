e747093c320e6921226be67f94af3db9920a13e1
Soumyadip-crypto <paruisoumyadip21@gmail.com>
2023-07-22 12:30:28 +0530

    upload

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,21 +14,34 @@ const Expenses = (props) => {
   const filteredExpenses = props.expenses.filter(
     (expense) => expense.date.getFullYear().toString() === filteredYear
   );
+  let expensesContent = <p>No expenses found.</p>;
+  if(filteredExpenses.length>0){
+    expensesContent =filteredExpenses.map((expense) => (
+      <ExpenseItem 
+        key={expense.id}
+        title={expense.title} 
+        amount={expense.amount} 
+        date={expense.date}
+      />
+    ));
+  }
+  if (filteredExpenses.length === 1) {
+    expensesContent = (
+      <div>
+        <p>Only single Expense here. Please add more...</p>
+        {expensesContent}
+      </div>
+    )}
   return (
     <div>
       <Card className="expenses">
-        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem 
-            key={expense.id}
-            title={expense.title} 
-            amount={expense.amount} 
-            date={expense.date}
-          />
-        ))}
+        <ExpensesFilter selected={filteredYear} 
+        onChangeFilter={filterChangeHandler} 
+        />
+        {expensesContent}
       </Card>
     </div>
   );
-}
+};
 
 export default Expenses;
